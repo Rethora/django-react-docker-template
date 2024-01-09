2a160e202112aea259deb5d245779586c2243e61
Dane Roelofs <dane.l.roelofs@gmail.com>
2024-01-09 06:51:16 +0000

    feat: :technologist: improve dev config

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 ## Development
 
+- cp example.env .env
 - Install vscode devcontainers extension
 - Reopen project in container
 - Start "Python: Django" from debugger
 - Start "Launch Chrome" from debugger
+- (optional) Open up new terminal
+  - Run `./manage.py createsuperuser`
 
 ## Deployment
 
--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
+
+load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&24ubb46zaej*fd9jz1^mw1t0)-@zd9g74f!hcs1a48-7loo0r"
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
